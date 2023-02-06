503da4c84c52a6708d1202231bb1c6da8953a919
AlexandrPopov <popov210784@mail.ru>
2023-02-06 19:11:16 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+var arr = GenerateArray(5, 5);
+PrintArray(arr);
+System.Console.WriteLine();
+Sum(arr);
+
+void Sum(int[,] arr)
+{
+    for(int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum = arr[i, j] + sum;
+        }
+        sum = sum / arr.GetLength(0);
+        System.Console.Write(sum + "; ");
+    }
+}
+
+int[,] GenerateArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 10);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
